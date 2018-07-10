6fc506a9a2c3d66d3c024ff860a0f8d78f104fb1
withoutmonkey <tyler@mediaslave.com>
2018-07-10 13:51:15 -0400

    initial spring boot 2 webflux sample

---
+++ boot-simple/src/main/java/org/tylernelson/spring/boot/simple/bootsimple/BootSimpleApplication.java
@@ -0,0 +1,12 @@
+package org.tylernelson.spring.boot.simple.bootsimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootSimpleApplication.class, args);
+	}
+}
